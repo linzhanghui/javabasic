ea4b023372c1db35c15a7209e212e4815ddcbe6b
linzhanghui <linzhanghui@gmail.com>
2014-10-10 15:14:49 +0800

    URL JAVA

---
+++ src/cn/jackie/net/ParseURL.java
@@ -0,0 +1,17 @@
+package cn.jackie.net;
+
+import java.net.URL;
+
+
+public class ParseURL {
+	public static void main(String[] args) throws Exception {
+		URL aURL = new URL("http://exmaple.com:80/docs/books/tutorial"+"/index.html?name=networking#DOWNLOADINGS");
+		System.out.println("protocol="+aURL.getProtocol());
+		System.out.println("authority="+aURL.getAuthority());
+		System.out.println("host="+aURL.getHost());
+		System.out.println("port="+aURL.getPort());
+		System.out.println("query="+aURL.getQuery());
+		System.out.println("filename="+aURL.getFile());
+		System.out.println("ref="+aURL.getRef());
+	}
+}
