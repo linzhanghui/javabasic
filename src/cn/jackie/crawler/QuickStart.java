a481eebbd4788eb9ab3e89147a388fdd64adcb93
linzhanghui <linzhanghui@gmail.com>
2015-03-19 15:50:28 +0800

    httpclient_start

---
+++ src/cn/jackie/crawler/QuickStart.java
@@ -0,0 +1,27 @@
+package cn.jackie.crawler;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+public class QuickStart {
+	public static void main(String[] args) throws Exception{
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+		try {
+			HttpGet httpGet = new HttpGet("http://www.163trade.com");
+			CloseableHttpResponse response1 = httpclient.execute(httpGet);
+			try {
+				System.out.println(response1.getStatusLine());
+				HttpEntity entity1 = response1.getEntity();
+				EntityUtils.consume(entity1);
+			} finally {
+				response1.close();
+			}
+		} finally {
+			httpclient.close();
+		}
+	}
+}
